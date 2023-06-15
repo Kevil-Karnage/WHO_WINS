6114d51d644ca53a1f2c797cc0b81d5050863643
Leo_Rozhnov <levlipetsk375@gmail.com>
2023-06-15 13:10:47 +0300

    Initialize Application

---
+++ src/main/java/com/rozhnov/who_wins/WhoWinsApplication.java
@@ -0,0 +1,13 @@
+package com.rozhnov.who_wins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhoWinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhoWinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rozhnov/who_wins/WhoWinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rozhnov.who_wins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhoWinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
