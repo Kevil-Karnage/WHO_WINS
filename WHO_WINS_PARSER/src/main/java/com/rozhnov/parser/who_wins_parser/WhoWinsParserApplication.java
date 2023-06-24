2d1c4c3fc17d4b3c8ba63a147f799c1c11ff1957
Leo_Rozhnov <levlipetsk375@gmail.com>
2023-06-24 19:06:18 +0300

    DEVELOP-12: initialize module WHO_WINS_PARSER

---
+++ WHO_WINS_PARSER/src/main/java/com/rozhnov/parser/who_wins_parser/WhoWinsParserApplication.java
@@ -0,0 +1,13 @@
+package com.rozhnov.parser.who_wins_parser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhoWinsParserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhoWinsParserApplication.class, args);
+    }
+
+}
